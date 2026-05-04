a442893b9eeddb0fca72af2df86aa70ebe464315
maykon-marcos-jr <maykon.marcos.junior.dev@gmail.com>
2026-05-04 19:55:38 -0300

    adding quality check

--- src/PRNG.py
+++ src/PRNG.py
@@ -31,9 +31,30 @@ Para que ambos os algoritmos gerem um número com a exata quantidade de bits esp
 cada execução deve apresentar o resultado do LCG e do BBS nessa ordem.
 
 """
+import math
+def monobit(bin_data: str):
+    """
+    Note that this description is taken from the NIST documentation [1]
+    [1] http://csrc.nist.gov/publications/nistpubs/800-22-rev1a/SP800-22rev1a.pdf
 
+    The focus of this test is the proportion of zeros and ones for the entire sequence. The purpose of this test is
+    to determine whether the number of ones and zeros in a sequence are approximately the same as would be expected
+    for a truly random sequence. This test assesses the closeness of the fraction of ones to 1/2, that is the number
+    of ones and zeros ina  sequence should be about the same. All subsequent tests depend on this test.
 
-def LCG(m: int, a: int, c: int, seed: int, nbits: int) -> int:
+    :param bin_data: a binary string
+    :return: the p-value from the test
+    """
+    count = 0
+    # If the char is 0 minus 1, else add 1
+    ones = bin_data.count('1')
+    count = ones - (len(bin_data) - ones)
+    # Calculate the p value
+    sobs = count / math.sqrt(len(bin_data))
+    p_val = math.erfc(math.fabs(sobs) / math.sqrt(2))
+    return p_val
+
+def LCG(m: int, a: int, c: int, seed: int, nbits: int) -> tuple[int, float]:
     """LCG:
     Linear Congruential Generator:
     - O LCG é um algoritmo clássico, extremamente rápido e simples, porém não é considerado seguro para fins criptográficos.
@@ -55,10 +76,11 @@ def LCG(m: int, a: int, c: int, seed: int, nbits: int) -> int:
         bits += str(lsb)
 
     rn = int(bits, 2)
-    return rn
+    p_value = monobit(bits)
+    return rn, p_value
 
 
-def BBS(p: int, q: int, seed: int, nbits: int) -> int:
+def BBS(p: int, q: int, seed: int, nbits: int) -> tuple[int, float]:
     """BBS:
     Blum Blum Shub:
     - O Algoritmo BBS se baseia em propriedades da teoria dos números e na dificuldade computacional de fatorar grandes números compostos.
@@ -82,7 +104,8 @@ def BBS(p: int, q: int, seed: int, nbits: int) -> int:
         bits += str(lsb)
 
     rn = int(bits, 2)
-    return rn
+    p_value = monobit(bits)
+    return rn, p_value
 
 
 def main(nbits):
@@ -131,13 +154,13 @@ def main(nbits):
     m = 2147483648
     a = 1103515245
     c = 12345
-    rn1 = LCG(m, a, c, seed, nbits)
+    rn1, pv1 = LCG(m, a, c, seed, nbits)
     p = 499
     q = 503
-    rn2 = BBS(p, q, seed, nbits)
+    rn2, pv2 = BBS(p, q, seed, nbits)
 
-    print(f"LCG -> {rn1}")
-    print(f"BBS -> {rn2}")
+    print(f"LCG -> {rn1} | {pv1}")
+    print(f"BBS -> {rn2} | {pv2}")
 
 if __name__ == "__main__":
     nbits = int(input())
